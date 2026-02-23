a7a34d986ab64245e7b5f23941dc0a395bd5bbd2
SarikaG12 <sarikasari1201@gmail.com>
2026-02-23 15:16:39 +0530

    [RAMCHARAN] Added UC1-Welcome Message

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,19 @@
+public class PalindroneCheckerApp {
+
+
+    /**
+     * Application entry point.
+     * <p>
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
+
